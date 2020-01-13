cd3126ab0923c46004eabb9fb8ad1606496ed1b1
Mark Floryan <floryan@d-172-27-99-85.dhcp.virginia.edu>
2020-01-13 15:51:42 -0500

    new sfx team hw

---
+++ homeworks/SFXLevelTeam/cameraAndSound/Camera.h
@@ -0,0 +1,20 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+class Camera{ 
+
+public:
+	Camera();
+	~Camera();
+
+	int x = 0;
+	int y = 0;
+	int viewportWidth = 500;
+	int viewportHeight = 500;
+
+private:
+
+	
+};
+
+#endif
---
+++ homeworks/SFXLevelTeam/cameraAndSound/Sound.h
@@ -0,0 +1,18 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+class Sound{ 
+
+public:
+	Sound();
+	~Sound();
+
+	void playSFX();
+	void playMusic();
+
+private:
+
+	
+};
+
+#endif
